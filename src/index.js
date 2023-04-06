3cd0138f77e023a217593c6b961c56040d098895
wddxg <48612121+wddxg@users.noreply.github.com>
2023-04-06 22:20:18 +0800

    Update index.js
    regroup

--- src/index.js
+++ src/index.js
@@ -37,18 +37,57 @@ export default {
     let appendObj = yaml.load(template.append);
     configObj = Object.assign(configObj, appendObj);
 
-    // replace proxy names
-    let proxyName = [];
+     // regroup
+    let proxyNames = {
+      HK: ["REJECT"],
+      TW: ["REJECT"],
+      SG: ["REJECT"],
+      JP: ["REJECT"],
+      US: ["REJECT"],
+      NF: ["REJECT"],
+      CG: ["REJECT"],
+      OTHERS: ["REJECT"],
+      ALL: ["REJECT"],
+      };
     configObj["proxies"].forEach((proxyElem) => {
-      proxyName.push(proxyElem["name"]);
+      let k = 0;
+      let name = proxyElem["name"]
+      if (name.match(/HK|hk|\u6e2f/)){
+        proxyNames.HK.push(name); k++;}
+      if (name.match(/TW|tw|\u53f0/)){
+        proxyNames.TW.push(name); k++;}
+      if (name.match(/SG|sg|\u72ee\u57ce|\u65b0\u52a0\u5761/)) {
+        proxyNames.SG.push(name); k++;}
+      if (name.match(/JP|jp|\u65e5\u672c/)){
+        proxyNames.JP.push(name); k++;}
+      if (name.match(/US|us|\u7f8e\u56fd/)){
+        proxyNames.US.push(name); k++;}
+      if (name.match(/Netflix|NF|\u89e3\u9501|\u7f51\u98de/)){
+        proxyNames.NF.push(name); k++;}
+      if (name.match(/gpt|GPT|Gpt|chat|Chat|CHAT/)){
+        proxyNames.CG.push(name); k++;}
+      if (k === 0){proxyNames.OTHERS.push(name);}
+      proxyNames.ALL.push(proxyElem["name"]);
     });
-
-    configObj["proxy-groups"].forEach((_, index) => {
-      let groupElem = configObj["proxy-groups"][index];
-
-      let i = groupElem["proxies"].indexOf("_PROXY_NAME");
-      if (i !== -1) {
-        groupElem["proxies"].splice(index, 1, ...proxyName);
+     // replace names
+    configObj["proxy-groups"].forEach((groupElem) => {
+      let proxiesArr = groupElem["proxies"];
+      let mapObj = {
+         "_PROXY_NAME_ALL": proxyNames.ALL,
+          "_PROXY_NAME_SG": proxyNames.SG,
+          "_PROXY_NAME_HK": proxyNames.HK,
+          "_PROXY_NAME_TW": proxyNames.TW,
+          "_PROXY_NAME_US": proxyNames.US,
+          "_PROXY_NAME_JP": proxyNames.JP,
+          "_PROXY_NAME_NF": proxyNames.NF,
+          "_PROXY_NAME_CG": proxyNames.CG,
+          "_PROXY_NAME_OT": proxyNames.OTHERS,
+      }
+      for (let key in mapObj) {
+        let i = proxiesArr.indexOf(key);
+        if (i !== -1) {
+          proxiesArr.splice(i-1, 1, ...mapObj[key]);
+        }
       }
     });
 
